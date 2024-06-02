082b252eca8bbd56630267b63e051cde88af8ea3
hamidurrk <hamidurrk@gmail.com>
2024-06-02 17:44:17 +0600

    first commit

---
+++ learningPhase/basics/2_dataTypesAndVar.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int a = 5;
+    float b = 5.5;
+    double c = 5.5;
+    bool d = true;
+    string s = "Hello";
+    char e = 'a';
+
+    cout << a << " " << b << " " << c << " " << d << " " << s << " " << e << endl;
+    return 0;
+}
